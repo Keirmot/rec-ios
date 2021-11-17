265299f16599f6a1b52f18466aaf1877a7782701
João Ferrão <jferrao@tripadvisor.com>
2021-11-17 12:22:47 +0000

    Initial commit

---
+++ rec/recApp.swift
@@ -0,0 +1,17 @@
+//
+//  recApp.swift
+//  rec
+//
+//  Created by João on 17/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct recApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
