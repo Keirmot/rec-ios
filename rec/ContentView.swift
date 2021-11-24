59a94c6fbe67f93f20ca2e7fc5cf1a9ec008ca9e
Keirmot <94868909+Keirmot@users.noreply.github.com>
2021-11-24 13:39:01 +0000

    Code Chalenge

--- rec/ContentView.swift
+++ rec/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State var isShowingMessage = false
+  var body: some View {
+    VStack {
+      Spacer()
+      Button(action: { isShowingMessage.toggle() }) {
+        Text("Press Here!")
+      }
+      .buttonStyle(.plain)
+      Spacer()
+      Text("Hello Tripadviser")
+        .opacity(isShowingMessage ? 1 : 0)
+        .padding(16)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
